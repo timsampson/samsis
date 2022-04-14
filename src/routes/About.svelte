e7095190546346215dcbc8b1690d2aab52b9a4a4
Tim Sampson <timsampson@gmail.com>
2022-04-14 19:52:51 +0800

    basic routing

---
+++ src/routes/About.svelte
@@ -0,0 +1,9 @@
+<h1 class="text-indigo-600 text-3xl font-bold ">About</h1>
+<p class="mt-1 py-2">
+  Trying out <a
+    class="underline text-blue-600 hover:text-blue-800 visited:text-purple-700"
+    href="https://github.com/ItalyPaleAle/svelte-spa-router/blob/master/examples/basic-routing/src/App.svelte"
+    >ItalyPaleAle\svelte-spa-router</a
+  >
+</p>
+<p class="mt-1">This is running on GAPS</p>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,14 @@
+<h1 class="text-indigo-600 text-3xl font-bold ">Home</h1>
+<p class="mt-1 py-2">
+  Visit the <a
+    class="underline text-blue-600 hover:text-blue-800 visited:text-purple-700"
+    href="https://svelte.dev/tutorial">Svelte tutorial</a
+  > to learn how to build Svelte apps.
+</p>
+<p class="mt-1">
+  Visit the <a
+    class="underline text-blue-600 hover:text-blue-800 visited:text-purple-700"
+    href="https://tailwindcss.com/blog/tailwindcss-v3">Tailwind CSS v3.0</a
+  > to learn about styling your app with TailwindCSS.
+</p>
+<p class="mt-1">This is running on GAPS</p>
