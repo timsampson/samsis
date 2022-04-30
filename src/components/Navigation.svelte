e82cb640d3b898fd3725dfeb28221c96460eb84f
Tim Sampson <timsampson@gmail.com>
2022-04-30 11:01:16 +0800

    Nav styling

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -36,28 +36,34 @@
   }
 </script>
 
-<nav class="pl-2 py-2 bg-slate-400">
-  <a
-    href="#/"
-    on:click={() => setURL("/home")}
-    class="hover:text-gray-300 mx-2 text-sm text-blue-800">Home</a
-  >
-  <a
-    href="#/about/"
-    on:click={() => setURL("/about")}
-    class="hover:text-gray-300 mx-2 text-sm text-blue-800">About</a
-  >
-  <a
-    href="#/clubsignup/"
-    on:click={() => setURL("/clubsignup")}
-    class="hover:text-gray-300 mx-2 text-sm text-blue-800">Club Sign Up</a
-  >
-  {#key userEmail}
-    <div
-      in:fade={{ duration: 1000 }}
-      class="rounded-full w-36 py-1 px-6 bg-blue-700 text-sm text-gray-300 border border-gray-300"
+<nav
+  class="flex items-center justify-between flex-wrap rounded-sm bg-gradient-to-r from-blue-400 to-blue-500 px-12 py-3"
+>
+  <div>
+    <a
+      href="#/"
+      on:click={() => setURL("/home")}
+      class="hover:text-gray-300 mx-2 text-sm text-blue-800">Home</a
     >
-      {userEmail.slice(0, userEmail.indexOf("@"))}
-    </div>
-  {/key}
+    <a
+      href="#/about/"
+      on:click={() => setURL("/about")}
+      class="hover:text-gray-300 mx-2 text-sm text-blue-800">About</a
+    >
+    <a
+      href="#/clubsignup/"
+      on:click={() => setURL("/clubsignup")}
+      class="hover:text-gray-300 mx-2 text-sm text-blue-800">Club Sign Up</a
+    >
+  </div>
+  <div>
+    {#key userEmail}
+      <div
+        in:fade={{ duration: 1000 }}
+        class="rounded-full py-1 px-6 bg-blue-700 text-sm text-gray-300 border border-gray-300"
+      >
+        {userEmail.slice(0, userEmail.indexOf("@"))}
+      </div>
+    {/key}
+  </div>
 </nav>
