47c723d80e364862370a0b8c782de85fc70d53cc
Tim Sampson <timsampson@gmail.com>
2022-05-24 18:28:05 +0800

    bind to component props

--- src/routes/ClubAdmin.svelte
+++ src/routes/ClubAdmin.svelte
@@ -78,42 +78,7 @@
 <h1 class="text-indigo-600 text-3xl font-bold ">Club Admin Page</h1>
 <p class="mt-1">Page for approving clubs.</p>
 <form on:submit|preventDefault={handleSubmit} id="clubAdmin">
-  <RadioAdmin />
-  <fieldset>
-    <div class="flex mt-2 ml-4">
-      <div class="form-check form-check-inline mr-4">
-        <input
-          class="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-600 bg-grey-200 checked:bg-green-600 checked:border-green-800 focus:outline-none transition duration-200 mt-1 align-top cursor-pointer"
-          type="radio"
-          name="approvalType"
-          id="approved_radio"
-          value="approved"
-          bind:group={approvalType}
-          required
-        />
-        <label
-          class="form-check-label inline-block cursor-pointer"
-          class:text-green-800={approvalType == "approved"}
-          for="approved_radio">Approved</label
-        >
-      </div>
-      <div class="form-check form-check-inline mr-4">
-        <input
-          class="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-600 bg-grey-200 checked:bg-red-600 checked:border-red-800 focus:outline-none transition duration-200 mt-1 align-top cursor-pointer"
-          type="radio"
-          name="approvalType"
-          id="rejected_radio"
-          value="rejected"
-          bind:group={approvalType}
-        />
-        <label
-          class="form-check-label inline-block cursor-pointer"
-          class:text-red-500={approvalType == "rejected"}
-          for="rejected_radio">Rejected</label
-        >
-      </div>
-    </div>
-  </fieldset>
+  <RadioAdmin bind:approvalType />
   {#if records.length > 0}
     <ul class="p-2 mx-auto" required>
       {#each records as record, i (record.recordId)}
