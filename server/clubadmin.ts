e60ad3ac5f8ba24bfb685f57a51bd8944a231e0a
Tim Sampson <timsampson@gmail.com>
2022-05-27 09:45:25 +0800

    remove club file, redundant

--- server/clubadmin.ts
+++
@@ -1,9 +0,0 @@
-function getClubApprovalRecords() {
-    clubApplicationValues = clubApplicationSheet.getDataRange().getValues();
-    clubApplicationValuesAsObjArray = ValuesToArrayOfObjects(clubApplicationValues);
-    let pendingClubs = clubApplicationValuesAsObjArray.filter((application) => application.processed == false);
-    pendingClubs.forEach((record, index) => {
-        pendingClubs[index] = JSON.stringify(record);
-    });
-    return pendingClubs;
-}
