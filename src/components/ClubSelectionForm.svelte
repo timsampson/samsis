888d38e74783b270b25b9f05598c8c45fde6d62c
Tim Sampson <timsampson@gmail.com>
2022-06-22 10:15:58 +0800

    remove custom btn

--- src/components/ClubSelectionForm.svelte
+++ src/components/ClubSelectionForm.svelte
@@ -110,7 +110,7 @@
           d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
         /></svg
       >
-      <span>clubFormState.applicationMessage</span>
+      <span>{clubFormState.applicationMessage}</span>
     </div>
   </div>
 {/if}
@@ -133,27 +133,9 @@
         {/each}
       </select>
     </div>
-    <button
-      type="submit"
-      class="bg-blue-500 inline-flex items-center my-4 py-2 px-4 font-bold text-white
-      hover:bg-blue-700 rounded-lg focus:shadow-outline disabled:opacity-50 disabled:bg-blue-300"
-      disabled={clubFormState.formIsClosed}
+
+    <button type="submit" class="btn btn-primary btn-sm mt-3" disabled={clubFormState.formIsClosed}
+      >submit</button
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-        />
-      </svg>
-      <span>Submit</span>
-    </button>
   </form>
 </div>
