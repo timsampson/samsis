7532432da9d9a582d797fa04a0ebd2fd6ff0f71d
Tim Sampson <timsampson@gmail.com>
2022-06-24 17:18:35 +0800

    search multiple columns

--- src/stores/meritStore.js
+++ src/stores/meritStore.js
@@ -1,8 +1,9 @@
 import { writable, derived } from 'svelte/store';
 export const term = writable('');
 export const items = writable([]);
-export const category = writable('student_name');
 export const filtered = derived(
-	[term, items, category], 
-	([$term, $items, $category]) => $items.filter(record => record[$category].toLowerCase().trim().includes($term))
+	[term, items], 
+	([$term, $items]) => $items.filter(record => {
+		return record.student_id.toLowerCase().trim().includes($term) || record.student_name.toLowerCase().trim().includes($term);
+	})
 );
