e244e384527783295910f393c8da5087ee9b30ce
Tim Sampson <timsampson@gmail.com>
2022-07-03 11:37:48 +0800

    Update property name

--- src/stores/meritStore.js
+++ src/stores/meritStore.js
@@ -1,4 +1,13 @@
 import { writable, derived } from 'svelte/store';
+export const categories = [ "Merit", "Information", "Level 1",  "Yellow Level",  "Orange Level", "Red Level", "Homework"];
+export const positiveList = ["helpful", "On task", "Diligent"];
+export const informationList = ["sleepy", "eating in class", "late", "emotional"];
+export const level1List = ["off task", "constantly chatting", "tardy"];
+export const YCList = ["shouting", "running", "sleeping"];
+export const OCList = ["fighting", "screaming", "thowing objects"];
+export const RCList = ["smoking", "fireworks", "swearing"];
+export const HWList = ["incomplete", "not submitted", "poor effort"];
+export const subjects = ["Reading"," History","Geography","Geometry","Algebra lI","Writing","Physics","Chemistry","Biology","Math","Precalculus","Calculus"];
 export const stepOneComplete = writable(false);
 export const meritDateValue = writable(() => new Date().toLocaleDateString("en-CA"));
 export const dateIsSelected = writable(false);
@@ -17,7 +26,7 @@ export const filtered = derived(
 			record.student_id.toLowerCase().trim().includes($meritSearchTerm) || 
 			record.student_name.toLowerCase().trim().includes($meritSearchTerm) || 
 			record.homeroom.toLowerCase().trim().includes($meritSearchTerm) || 
-			record.hr_teacher.toLowerCase().trim().includes($meritSearchTerm) || 
+			record.hr_teacher_name.toLowerCase().trim().includes($meritSearchTerm) || 
 			record.grade.toString().toLowerCase().trim().includes($meritSearchTerm)
 			);
 	})
