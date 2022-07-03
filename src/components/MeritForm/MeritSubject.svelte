103d4ac8d847d7d2b6b49b98a8fba186f90e1c8a
Tim Sampson <timsampson@gmail.com>
2022-07-03 11:40:32 +0800

    add merit subject component

---
+++ src/components/MeritForm/MeritSubject.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { subjects } from "../../stores/meritStore.js";
+</script>
+
+<div class="form-control w-full max-w-xs">
+  <label for="subject-select" class="label">
+    <span class="label-text">Select the subject:</span>
+  </label>
+  <select id="subject-select" class="select select-bordered select-primary" required>
+    <option value="">-- Please choose --</option>
+    {#each subjects as subject}
+      <option value={subject}>{subject}</option>
+    {/each}
+  </select>
+</div>
