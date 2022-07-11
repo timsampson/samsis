e5ff2411cda5b6209a635590754e92151008eea8
Tim Sampson <timsampson@gmail.com>
2022-07-11 14:56:51 +0800

    add admin table

---
+++ src/components/MeritForm/MeritAdminTable.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { fade } from "svelte/transition";
+  import { meritAdminValues } from "../../stores/meritAdminStore.js";
+</script>
+
+<div class="overflow-x-auto">
+  <table class="table table-compact w-full table-zebra" in:fade|local={{ duration: 1000 }}>
+    <thead>
+      <tr>
+        <th>Date</th>
+        <th>Student</th>
+        <th>Teacher</th>
+        <th>Category</th>
+        <th>Type</th>
+        <th>Details</th>
+      </tr>
+    </thead>
+    {#if $meritAdminValues.length > 0}
+      <tbody>
+        {#each $meritAdminValues as merit}
+          <tr>
+            <td>{merit.timestamp.toLocaleDateString()}</td>
+            <td>{merit.student_name}</td>
+            <td>{merit.teacher_name}</td>
+            <td>{merit.categories}</td>
+            <td>{merit.type}</td>
+            <td>{merit.all_details}</td>
+          </tr>
+        {/each}
+      </tbody>
+      <tfoot>
+        <tr>
+          <th>Date</th>
+          <th>Student</th>
+          <th>Teacher</th>
+          <th>Category</th>
+          <th>Type</th>
+          <th>Details</th>
+        </tr>
+      </tfoot>
+    {/if}
+  </table>
+</div>
